6413ee978281cf4c233f63b9f5b7b050ad7a07b4
ChoiDongHyun <95002650+CDHyun@users.noreply.github.com>
2023-05-04 21:11:55 +0900

    JDBC 연결, ShareVar, gitignore 추가
    JDBC MySQL Connector Library 추가, ShareVar Class, gitignore 추가

---
+++ src/com/javalec/util/ShareVar.java
@@ -0,0 +1,13 @@
+package com.javalec.util;
+
+public class ShareVar {
+
+	/* DB위치 */
+//	public static final String DBName = "jdbc:mysql://127.0.0.1/shoemarket?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	public static final String DBName = "jdbc:mysql://192.168.0.100/shoemarket?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	/* DB 계정 */
+	public static final String DBUser = "root";
+	/* DB 비밀번호 */
+	public static final String DBPass = "qwer1234";
+	
+}
