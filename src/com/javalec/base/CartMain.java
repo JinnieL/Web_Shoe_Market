117eef3253dda50c2a3fbc18416994d24d6e8487
kangdaegyu <>
2023-05-05 00:01:19 +0900

    Cart Design 구현, Dao, Dto 생성
    window가 열릴 때 화면 테이블 헤더까지 구현

--- src/com/javalec/base/CartMain.java
+++ src/com/javalec/base/CartMain.java
@@ -5,10 +5,35 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class CartMain extends JFrame {
 
 	private JPanel contentPane;
+	private JScrollPane scrollPane;
+	private JTable innerTable;
+	private JLabel lblNewLabel;
+	private JTextField textField;
+	private JButton btnEmpty;
+	private JButton btnOrder;
+	private JButton btnModify;
+	private JButton btnCancel;
+	private JButton btnMain;
+	
+	// Table
+	private final DefaultTableModel outerTable = new DefaultTableModel();
 
 	/**
 	 * Launch the application.
@@ -30,12 +55,163 @@ public class CartMain extends JFrame {
 	 * Create the frame.
 	 */
 	public CartMain() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				tableInit();
+			}
+		});
+		setTitle("Cart");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 502, 337);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(getScrollPane());
+		contentPane.add(getLblNewLabel());
+		contentPane.add(getTextField());
+		contentPane.add(getBtnEmpty());
+		contentPane.add(getBtnOrder());
+		contentPane.add(getBtnModify());
+		contentPane.add(getBtnCancel());
+		contentPane.add(getBtnMain());
 	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(6, 6, 490, 223);
+			scrollPane.setViewportView(getInnerTable());
+		}
+		return scrollPane;
+	}
+	
+	// table 정의 innerTable + outerTable, 이미지 사이즈
+	private JTable getInnerTable() {
+		if (innerTable == null) {
+			innerTable = new JTable() {									// 테이블 데이터 지정
+				public Class getColumnClass(int column) {				// 속성을 바꿔준다.
+					return (column == 0) ? Icon.class : Object.class;	// 0번째 컬럼은 이미지 나머지는 오브젝트로 넣는다
+				}
+				
+			};
+			innerTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			innerTable.setModel(outerTable);	
+			innerTable.setRowHeight(150);
+		}
+		
+		return innerTable;
+	}
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("총 합계 :");
+			lblNewLabel.setBounds(317, 238, 53, 16);
+		}
+		return lblNewLabel;
+	}
+	private JTextField getTextField() {
+		if (textField == null) {
+			textField = new JTextField();
+			textField.setBounds(366, 233, 130, 26);
+			textField.setColumns(10);
+		}
+		return textField;
+	}
+	private JButton getBtnEmpty() {
+		if (btnEmpty == null) {
+			btnEmpty = new JButton("장바구니 비우기");
+			btnEmpty.setBounds(370, 266, 130, 29);
+		}
+		return btnEmpty;
+	}
+	private JButton getBtnOrder() {
+		if (btnOrder == null) {
+			btnOrder = new JButton("주문");
+			btnOrder.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			btnOrder.setBounds(0, 266, 85, 29);
+		}
+		return btnOrder;
+	}
+	private JButton getBtnModify() {
+		if (btnModify == null) {
+			btnModify = new JButton("수량수정");
+			btnModify.setBounds(82, 266, 85, 29);
+		}
+		return btnModify;
+	}
+	private JButton getBtnCancel() {
+		if (btnCancel == null) {
+			btnCancel = new JButton("취소");
+			btnCancel.setBounds(164, 266, 85, 29);
+		}
+		return btnCancel;
+	}
+	private JButton getBtnMain() {
+		if (btnMain == null) {
+			btnMain = new JButton("메인");
+			btnMain.setBounds(246, 266, 85, 29);
+		}
+		return btnMain;
+	}
+	
+	// ================= function ===================
+	
+	private void tableInit() {
+		outerTable.addColumn("상품사진");	// 타이틀 네임
+		outerTable.addColumn("상품명");
+		outerTable.addColumn("수량");
+		outerTable.addColumn("가격");		
+		outerTable.setColumnCount(4);		// 타이틀이 몇개냐
+	
+		int i = outerTable.getRowCount();	// 테이블에 데이터가 몇개 있는지
+		
+		for(int j=0; j<i;j++) {
+			outerTable.removeRow(0);		// 지워주기
+		}
+		
+		innerTable.setAutoResizeMode(innerTable.AUTO_RESIZE_OFF); // 사이즈 조절 안한다
+		
+		// 상품이미지 사이즈
+		int vColIndex = 0;					// 데이터 크기 조절 
+		TableColumn col = innerTable.getColumnModel().getColumn(vColIndex);
+		int width = 200;
+		col.setPreferredWidth(width);
+		
+		// 상품명 사이즈
+		vColIndex =1;
+		col = innerTable.getColumnModel().getColumn(vColIndex);
+		width = 150;
+		col.setPreferredWidth(width);
+		
+		// 수량 사이즈
+		vColIndex =2;
+		col = innerTable.getColumnModel().getColumn(vColIndex);
+		width = 40;
+		col.setPreferredWidth(width);
+		
+		// 가격 사이즈
+		vColIndex =3;
+		col = innerTable.getColumnModel().getColumn(vColIndex);
+		width = 100;
+		col.setPreferredWidth(width);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // end
 
-}
