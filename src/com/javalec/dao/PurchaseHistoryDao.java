0f0407391e9061748c312cbfb333fed4a1f25ec0
Donghyun <pdchoirian@gmail.com>
2023-05-07 02:42:12 +0900

    주문 내역 페이지 구현, Insertdate에서 시간 빼고 날짜만 출력 해주는 기능 구현

---
+++ src/com/javalec/dao/PurchaseHistoryDao.java
@@ -0,0 +1,87 @@
+package com.javalec.dao;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.javalec.dto.ProductDto;
+import com.javalec.dto.PurchaseDto;
+import com.javalec.dto.PurchaseHistoryDto;
+import com.javalec.util.ShareVar;
+
+public class PurchaseHistoryDao {
+	/* Field */
+	private final String url_mysql = ShareVar.DBName;
+	private final String id_mysql = ShareVar.DBUser;
+	private final String pw_mysql = ShareVar.DBPass;
+	int purchaseNo;
+	int productCode;
+	int productStock;
+	int size;
+	String userid = "donghyun";
+	int purchaseQty;
+	String purchaseInsertdate;
+	String purchaseDeletedate;
+	String productName;
+	String productImageName;
+	int productPrice;
+	String wkImageName;
+	
+	
+	/* Constructor */
+	public PurchaseHistoryDao() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	/* Method */
+	/* 01. 주문 내역 데이터를 검색하는 메소드 */
+	public ArrayList<PurchaseHistoryDto> getPurchaseHistory(String userid){
+		ArrayList<PurchaseHistoryDto> beanList = new ArrayList<PurchaseHistoryDto>();
+		String query = "select p.purchaseNo, pd.productName, pd.productPrice, p.purchaseQty, p.purchaseInsertdate, pd.productImageName, pd.productImage"
+				+ " from user u, purchase p, product pd"
+				+ " where u.userid = p.userid and p.productCode = pd.productCode and"
+				+ " u.userid = " + "'" + userid + "'";
+	
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url_mysql, id_mysql, pw_mysql);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while(rs.next()) {
+				int wkNo = rs.getInt(1);
+				String wkName = rs.getString(2);
+				int wkPrice = rs.getInt(3);
+				int wkQty = rs.getInt(4);
+				String wkDate = rs.getString(5);
+				wkImageName = rs.getString(6);
+				System.out.println(wkImageName);
+				File file = new File("./" + wkImageName);
+				FileOutputStream output = new FileOutputStream(file);
+				InputStream input = rs.getBinaryStream(7);
+				byte[] buffer = new byte[1024];
+				while(input.read(buffer) > 0) {
+					output.write(buffer);
+				}
+				PurchaseHistoryDto historyDto = new PurchaseHistoryDto(wkNo, wkQty, wkDate, wkName, wkImageName, wkPrice);
+				beanList.add(historyDto);
+			}
+			con.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return beanList;
+	}
+	
+	
+	
+	
+}	// End Class
